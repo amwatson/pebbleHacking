6bf5b78986c50ba115a7089721562eb1b8604571
Amanda Watson <amwatson@andrew.cmu.edu>
2015-01-20 15:25:56 -0500

    Added code

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,70 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // Construct URL
+  var url = "http://www.andrew.cmu.edu/user/amwatson/website/test.html";
+  console.log("url is" + url);
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      // Conditions
+      var conditions = json.main.condition;      
+      console.log("Conditions are " + conditions);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        "KEY_CONDITIONS": conditions,
+      };
+
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Weather info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending weather info to Pebble!");
+        }
+      );
+    }      
+  );
+}
+
+function locationError(err) {
+  console.log("Error requesting location!");
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial weather
+    getWeather();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getWeather();
+  }                     
+);
