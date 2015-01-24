68267949e0cf2c060fc64a9e6e982ab9fdc1a6c2
Amanda Watson <amwatson@andrew.cmu.edu>
2015-01-24 14:13:45 -0800

    Added buttons

--- src/main.c
+++ src/main.c
@@ -4,12 +4,6 @@
 #define KEY_TIME 0
 #define KEY_TICKS 1
   
-#define NUM_MENU_SECTIONS 1
-#define NUM_MENU_ICONS 1
-#define NUM_FIRST_MENU_ITEMS 1
-#define NUM_SECOND_MENU_ITEMS 0
-
-int menu_select = 0;
 static Window *s_main_window;
 static Window *s_field_window;
 static TextLayer *s_time_layer;
@@ -42,111 +36,38 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
-// This is a menu layer
-// You have more control than with a simple menu layer
-static MenuLayer *menu_layer;
+static void main_up_click_handler(ClickRecognizerRef recognizer, void *context) {
 
-// Menu items can optionally have an icon drawn with them
-static GBitmap *menu_icons[NUM_MENU_ICONS];
-
-static int current_icon = 0;
-
-// You can draw arbitrary things in a menu item such as a background
-static GBitmap *menu_background;
-
-// A callback is used to specify the amount of sections of menu items
-// With this, you can dynamically add and remove sections
-static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
-  return NUM_MENU_SECTIONS;
 }
 
-// Each section has a number of items;  we use a callback to specify this
-// You can also dynamically add and remove items using this
-static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  switch (section_index) {
-    case 0:
-      return NUM_FIRST_MENU_ITEMS;
-
-    case 1:
-      return NUM_SECOND_MENU_ITEMS;
-
-    default:
-      return 0;
-  }
+static void main_down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  
 }
 
-// A callback is used to specify the height of the section header
-static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  // This is a define provided in pebble.h that you may use for the default height
-  return MENU_CELL_BASIC_HEADER_HEIGHT;
-}
-
-// Here we draw what each header is
-static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
-  // don't use this right now
-}
-
-// This is the menu item draw callback where you specify what each item should look like
-static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
-  // Determine which section we're going to draw in
-  switch (cell_index->section) {
-    case 0:
-      // Use the row to specify which item we'll draw
-      switch (cell_index->row) {
-        case 0:
-          // This is a basic menu item with a title and subtitle
-          menu_cell_basic_draw(ctx, cell_layer, "Enter Time", "unlock screen", NULL);
-          break;
-      }
-      break;
-
-  }
-}
-
-// Here we capture when a user selects a menu item
-void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-  // Use the row to specify which item will receive the select action
-  switch (cell_index->row) {
-    // This is the menu item with the cycling icon
-    case 1:
-      // Cycle the icon
-      current_icon = (current_icon + 1) % NUM_MENU_ICONS;
-      // After changing the icon, mark the layer to have it updated
-      layer_mark_dirty(menu_layer_get_layer(menu_layer));
-      break;
-  }
+static void main_select_raw_down_handler(ClickRecognizerRef recognizer, void *context) {
+  // Show the Window on the watch, with animated=true
   window_stack_push(s_field_window, true);
+}
+static void main_select_raw_up_handler(ClickRecognizerRef recognizer, void *context) {
+  
+}
+
+static void main_back_handler(ClickRecognizerRef recognizer, void *context) {
+  
+}
+
+
+static void main_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_UP, main_up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, main_down_click_handler);
+  window_raw_click_subscribe(BUTTON_ID_SELECT, main_select_raw_down_handler, main_select_raw_up_handler, NULL);
+  window_single_click_subscribe(BUTTON_ID_BACK, main_back_handler);
 }
 static void field_window_load(Window *window) {
  // Here we load the bitmap assets
   // resource_init_current_app must be called before all asset loading
-  int num_menu_icons = 0;
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WHITE);
+  window_set_fullscreen(s_field_window, true);
 
-  // And also load the background
-  menu_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_frame(window_layer);
-  
-  // Create the menu layer
-  menu_layer = menu_layer_create(bounds);
-
-  // Set all the callbacks for the menu layer
-  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_sections = menu_get_num_sections_callback,
-    .get_num_rows = menu_get_num_rows_callback,
-    .get_header_height = menu_get_header_height_callback,
-    .draw_header = menu_draw_header_callback,
-    .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
-  });
-
-  // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(menu_layer, window);
-
-  // Add it to the window for display
-  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
-  
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(5, 75, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
@@ -167,36 +88,6 @@ static void field_window_load(Window *window) {
 }
 
 static void main_window_load(Window *window) {
-  
-  
-   // Here we load the bitmap assets
-  // resource_init_current_app must be called before all asset loading
-  int num_menu_icons = 0;
-  menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WHITE);
-
-  // And also load the background
-  menu_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_frame(window_layer);
-  
-  // Create the menu layer
-  menu_layer = menu_layer_create(bounds);
-
-  // Set all the callbacks for the menu layer
-  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
-    .get_num_sections = menu_get_num_sections_callback,
-    .get_num_rows = menu_get_num_rows_callback,
-    .get_header_height = menu_get_header_height_callback,
-    .draw_header = menu_draw_header_callback,
-    .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
-  });
-
-  // Bind the menu layer's click config provider to the window for interactivity
-  menu_layer_set_click_config_onto_window(menu_layer, window);
-
-  // Add it to the window for display
-  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
   
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(5, 75, 139, 50));
@@ -330,6 +221,8 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+  
+  window_set_click_config_provider(s_main_window, main_config_provider);
   
   window_set_window_handlers(s_field_window, (WindowHandlers) {
     .load = field_window_load,
